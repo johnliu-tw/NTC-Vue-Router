c2c6eb5b5e599b6527c7c4abb1bc05c0fb365d6c
johnliu <johnliutw@hotmail.com>
2022-02-20 11:40:58 +0800

    demo: add new view

---
+++ src/views/TestView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="This is a TestView"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'TestView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
